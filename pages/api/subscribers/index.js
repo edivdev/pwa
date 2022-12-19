915aa2646451d4c88d6ba1ff4157e05a98c2736f
Felipe Estrada <felipe.estradagomez@gmail.com>
2022-12-19 07:16:20 -0500

    modified email for subscribers

--- pages/api/subscribers/index.js
+++ pages/api/subscribers/index.js
@@ -1,27 +1,27 @@
-import axios from "axios";
+import { transporter } from "../../../lib/emailService/transport";
 
 export default async function handler(req, res) {
-  try {
-    const reqAuth = await axios.post(
-      process.env.BACKEND_URL + "/api/auth/local",
-      {
-        identifier: process.env.WEBSITE_USER,
-        password: process.env.WEBSITE_USER_PASSWORD,
-      }
-    );
+  if (req.method === "POST") {
+    const { fname, lname, email } = req.body.data;
 
-    const data = await axios.post(
-      process.env.BACKEND_URL + "/api/subscribers",
-      req.body,
-      {
-        headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + reqAuth.data.jwt,
-        },
-      }
-    );
-    return res.status(200).json(data);
-  } catch (error) {
-    res.json(error);
+    const mailData = {
+      from: "EDUCACION DIVERSA WEBSITE",
+      to: process.env.CONTACT_EMAIL_TARGET_ADDRESS,
+      subject: `New susbscriber to our NEWSLETTER`,
+      html: `
+      <div>
+        This is a new request subscription for newsletter from ED website<br/><br/>
+        Fullname: ${fname} ${lname}<br/>
+        Email: ${email}
+      </div>
+      `,
+    };
+
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) console.log(err);
+      else console.log(info);
+    });
+
+    return res.status(200);
   }
 }
